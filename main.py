4a18bed89894059741ab8b6fec18f82a799c38c9
KaanCicekci <kanacilek2@gmail.com>
2024-10-25 22:41:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print ("hello world")
+import random
+karakterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+uzunluk = int(input("Lütfen Bir Uzunluk Girin:"))
+b = random.choice(karakterler)
+parola = "".join(random.sample(karakterler,uzunluk))
+print("işte yeni parolan " + parola)
